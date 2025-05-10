68c1f2c7d5503573dd1a04d2cb8f184f371287bc
Szedann <65607498+Szedann@users.noreply.github.com>
2025-05-10 16:45:39 +0200

    Initial commit

---
+++ fabric/src/main/java/dev/szedann/createBluemap/fabric/client/CreateBluemapFabricClient.java
@@ -0,0 +1,10 @@
+package dev.szedann.createBluemap.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class CreateBluemapFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
